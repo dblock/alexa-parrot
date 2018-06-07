4061bef0264c139cbcec163f4ba9fe1cc1f549c0
Jeroen Claassens <jeroen.claassens@live.nl>
2018-06-07 14:01:33 +0200

    better skills output

--- functions/parrot/skill.js
+++ functions/parrot/skill.js
@@ -1,42 +1,7 @@
 const parrot = require('./parrot');
-const schema = JSON.parse(parrot.schema());
-const utterances = parrot.utterances().split('\n');
 
-Array.prototype.clean = function (deleteValue) {
-  for (let i = 0; i < this.length; i++) {
-    if (this[i] === deleteValue) {
-      this.splice(i, 1);
-      i--;
-    }
-  }
-
-  return this;
-};
-
-for (let i = 0; i < schema.intents.length; ++i) {
-  const slots = [];
-  const samples = [];
-
-  schema.intents[0].slots.forEach((slot) => {
-    slots.push({
-      name: slot.name,
-      type: slot.type
-    });
-  });
-
-  utterances.clean('').forEach((utter) => {
-    const splitUtter = utter.split(' '); // eslint-disable-line newline-after-var
-    samples.push(`${splitUtter[1]} ${splitUtter[2]}`);
-  });
-
-  console.dir({
-    name: schema.intents[i].intent,
-    slots: [slots],
-    samples: [samples]
-  }, {
-    showHidden: true,
-    depth: null,
-    colors: true
-  });
-  console.log();
-}
+console.log('Intent Schema:');
+console.log();
+console.log(parrot.schemas.skillBuilder());
+console.log('Utterances');
+console.log(parrot.utterances());
