f29748ba6b52ad6536509938691fb52d5bb14fb0
Jeroen Claassens <jeroen.claassens@live.nl>
2018-06-07 03:24:10 +0200

    output schema to new intent JSON editor format

--- functions/parrot/skill.js
+++ functions/parrot/skill.js
@@ -1,9 +1,41 @@
-var parrot = require('./parrot');
+const parrot = require('./parrot');
+const schema = JSON.parse(parrot.schema());
+const utterances = parrot.utterances().split('\n');
 
-console.log("SCHEMA: ");
-console.log(parrot.schema());
+Array.prototype.clean = function (deleteValue) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == deleteValue) {
+            this.splice(i, 1);
+            i--;
+        }
+    }
+    return this;
+};
 
-console.log();
-console.log("UTTERANCES: ");
-console.log(parrot.utterances());
+for (let i = 0; i < schema.intents.length; ++i) {
+    let slots = [];
+    let samples = [];
 
+    schema.intents[0].slots.forEach((slot) => {
+        slots.push({
+            name: slot.name,
+            type: slot.type
+        })
+    });
+
+    utterances.clean('').forEach((utter) => {
+        splitUtter = utter.split(' ');
+        samples.push(`${splitUtter[1]} ${splitUtter[2]}`);
+    })
+
+    console.dir({
+        name: schema.intents[i].intent,
+        slots: [slots],
+        samples: [samples]
+    }, {
+        showHidden: true,
+        depth: null,
+        colors: true
+    })
+    console.log();
+}
